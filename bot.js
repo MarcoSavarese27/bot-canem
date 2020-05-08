fb0972c0e041801f99fd30c38ced03b37649f797
MarcoSavarese27 <64870788+MarcoSavarese27@users.noreply.github.com>
2020-05-08 19:10:26 +0200

    Second Version
    Now it answers to a given input message

--- bot.js
+++ bot.js
@@ -8,5 +8,12 @@ client.on('ready', () => {
     console.log('This bot is online!');
 })
 
+client.on('message', msg=>{
+    if (msg.content == "Ciao Bot" || msg.content == "ciao" || msg.content == "Ciao" || msg.content == "ciao Canem" || msg.content == "Ciao Canem" || msg.content == "ciao canem"){
+        msg.reply ('Ciao amico di Canem Reborn');
+    }
+})
+
+
 client.login(token);
         
